784dd43bc61d08c88ed7c1f6e36cfa2f35bde730
gbd850 <mmyszaki@gmail.com>
2024-02-17 17:20:10 +0100

    Add negative scenario integration test

--- src/test/java/com/test/atipera/integration/RepoControllerTest.java
+++ src/test/java/com/test/atipera/integration/RepoControllerTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -32,10 +32,27 @@ public class RepoControllerTest {
         request
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[*].repositoryName").value(anyString()))
-                .andExpect(jsonPath("$[*].ownerLogin").value(anyString()))
-                .andExpect(jsonPath("$[*].branches").isArray())
-                .andExpect(jsonPath("$[*].branches[*].name").value(anyString()))
-                .andExpect(jsonPath("$[*].branches[*].lastCommitSha").value(anyString()));
+                .andExpect(jsonPath("$[0].repositoryName").isString())
+                .andExpect(jsonPath("$[0].ownerLogin").isString())
+                .andExpect(jsonPath("$[0].branches").isArray())
+                .andExpect(jsonPath("$[0].branches[0].name").isString())
+                .andExpect(jsonPath("$[0].branches[0].lastCommitSha").isString());
+    }
+
+    @Test
+    public void givenInvalidUsername_whenRequest_thenStatus404() throws Exception {
+        // given
+        String username = " ";
+        // when
+        ResultActions request = mvc.perform(
+                get(String.format("/api/repos/%s", username))
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+        // then
+        request
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value(HttpStatus.NOT_FOUND.value()))
+                .andExpect(jsonPath("$.message").value("User with given username does not exist"));
     }
 }
