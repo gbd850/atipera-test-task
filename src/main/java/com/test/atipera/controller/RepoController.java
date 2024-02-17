ca419f6435130ac5c4389803198e90c7a9008deb
gbd850 <mmyszaki@gmail.com>
2024-02-17 15:43:40 +0100

    Edit controller response

--- src/main/java/com/test/atipera/controller/RepoController.java
+++ src/main/java/com/test/atipera/controller/RepoController.java
@@ -1,5 +1,7 @@
 package com.test.atipera.controller;
 
+import com.test.atipera.model.Branch;
+import com.test.atipera.model.Repo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +9,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/repos")
 public class RepoController {
 
     @GetMapping("{username}")
-    public ResponseEntity getUserRepositories(@PathVariable String username) {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<List<Repo>> getUserRepositories(@PathVariable String username) {
+        return new ResponseEntity<>(List.of(new Repo("", "", List.of(new Branch("", "")))), HttpStatus.OK);
     }
 
 }
